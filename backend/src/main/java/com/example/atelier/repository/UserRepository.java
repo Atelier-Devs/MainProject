eeb668bc17fec498e90310bf0bb884ad73cbe204
SangYeob <Superkim9302@naver.com>
2025-03-10 20:18:52 +0900

    최종 수정 0310

--- backend/src/main/java/com/example/atelier/repository/UserRepository.java
+++ backend/src/main/java/com/example/atelier/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface  UserRepository extends  JpaRepository<User,Long>{
     boolean existsByEmail(String email); // email 중복 확인
     User findByEmail(String email); // 이메일로 사용자 조회
 }
