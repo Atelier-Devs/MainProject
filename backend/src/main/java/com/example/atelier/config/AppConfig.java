b2d1dbee98242b30cf7c2508e47b7abda1af41e9
Hang-cod <songaaa375@naver.com>
2025-03-19 19:25:08 +0900

    OrderController 및 PaymentController 수정

---
+++ backend/src/main/java/com/example/atelier/config/AppConfig.java
@@ -0,0 +1,4 @@
+package com.example.atelier.config;
+
+public class AppConfig {
+}
--- backend/src/main/java/com/example/atelier/controller/OrderController.java
+++ backend/src/main/java/com/example/atelier/controller/OrderController.java
@@ -27,12 +27,13 @@ public class OrderController {
     private OrderRepository orderRepository;
     private ModelMapper modelMapper;
 
-    // 주문 생성
+//     주문 생성
     @PostMapping("/register")
     public ResponseEntity<Integer> createOrder(@RequestBody OrderDTO orderDTO) {
         int orderId = orderService.createOrder(orderDTO);
         return ResponseEntity.ok(orderId);
-    }
+   }                    // 결제 후 자동 생성되므로 생성할 필요없음.
+
 
     // 주문 조회(직원,관리자모드)
     @GetMapping("/{id}")
@@ -63,14 +64,24 @@ public class OrderController {
         return ResponseEntity.ok(orderList);
     }
 
-    // 여러 개의 주문 상태 및 아이템 수정
-    @PutMapping("/modify/{userId}")
-    public ResponseEntity<List<OrderDTO>> modifyOrders(@RequestBody List<OrderDTO> orderDTOList, @PathVariable Integer userId) {
-        List<OrderDTO> modifiedOrders = orderService.modifyOrder(orderDTOList, userId);
+    // 사용자 환불 요청 API
+    @PostMapping("/{orderId}/request-refund")
+    public ResponseEntity<String> requestRefund(
+            @PathVariable Integer orderId,
+            @RequestParam Integer userId) {
 
-        if (modifiedOrders.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.emptyList());
-        }
-        return ResponseEntity.ok(modifiedOrders);
+        orderService.requestRefund(orderId, userId);
+        return ResponseEntity.ok("환불 요청이 성공적으로 접수되었습니다.");
     }
+
+    // 여러 개의 주문 상태 및 아이템 수정
+//    @PutMapping("/modify/{userId}")
+//    public ResponseEntity<List<OrderDTO>> modifyOrders(@RequestBody List<OrderDTO> orderDTOList, @PathVariable Integer userId) {
+//        List<OrderDTO> modifiedOrders = orderService.modifyOrder(orderDTOList, userId);
+//
+//        if (modifiedOrders.isEmpty()) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.emptyList());
+//        }
+//        return ResponseEntity.ok(modifiedOrders);
+//    } 결제 내역을 수정할 일이 없음.
 }
