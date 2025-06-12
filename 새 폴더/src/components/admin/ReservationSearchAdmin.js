f4e2ca27ca4908914337d05c3f46a8c1cfc3a92e
SangYeob <Superkim9302@naver.com>
2025-06-12 19:00:15 +0900

    최종

--- 새 폴더/src/components/admin/ReservationSearchAdmin.js
+++
@@ -1,7 +0,0 @@
-// src/api/reservationApi.js
-import axios from "axios";
-
-export const fetchAllReservations = async () => {
-  const res = await axios.get("/api/atelier/reservations/list");
-  return res.data;
-};
